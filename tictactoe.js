01e837310ebb434a5366baf887b1afeb94d064a3
Gagan Jakhotiya <gagan@coverfoxmail.com>
2017-04-13 01:17:10 +0530

    I need a reverse mapper too. *tsk tsk*

--- tictactoe.js
+++ tictactoe.js
@@ -44,7 +44,9 @@ function prepareScoreArray(grid){
     scores = []
     for (let rowIndex = 0; rowIndex < size; rowIndex++) {
         for (let colIndex = 0; colIndex < size; colIndex++) {
-            
+            getArrayIndicesForCoordinates(rowIndex, colIndex, size).forEach(index => {
+                scores[index] = (scores[index] || 0) + UNIT_VAL[grid[rowIndex][colIndex]]
+            })
         }
     }
 }
@@ -59,4 +61,15 @@ function getArrayIndicesForCoordinates(row, col, size) {
     if (row + col + 1 === size)
         impactedIndices.push((2 * size) + 1)
     return impactedIndices
+}
+
+function getFirstCoordinatesFromArrayIndex(index, size) {
+    if (index === 2 * size)
+        return [0,0]
+    if (index === (2 * size) + 1)
+        return [0, size - 1]
+    if (index % 2 === 0)
+        return [0, index / 2]
+    else
+        return [parseInt(index / 1), 0]    
 }
